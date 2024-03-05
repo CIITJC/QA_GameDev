567b3c0ad2e1c60806a57175a02fbbe98fdc6862
CIITSHSDTP57 <123520879+rayos-ciit@users.noreply.github.com>
2024-03-05 09:58:30 +0800

    QA Dev
    No code yet

---
+++ Assets/scripts/script.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+using TMPro;
+
+public class script : MonoBehaviour
+{
+
+    public GameObject scene_1, scene_2, scene_3;
+    public GameObject stats;
+    public TextMeshProUGUI healthValue, sanityValue;
+    public int healthpoints, sanitypoints;
+
+    public GameObject[] Scenes;
+    public int ActiveScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scenes[0].SetActive(true);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SceneChnager()
+    {
+        ActiveScene = 1;
+        Scenes[1].SetActive(true);
+        for (int i = 0; i < Scenes.Length; i++)
+        {
+            Scenes[i].SetActive(i == ActiveScene);
+        }
+    }
+    public void StartButton()
+    {
+        //scene_1.SetActive(false);
+        //scene_2.SetActive(true);
+        //stats.SetActive(true);
+        ActiveScene = 1;
+        Scenes[1].SetActive(true);
+        for (int i = 0; i < Scenes.Length; i ++)
+        {
+            Scenes[i].SetActive(i == ActiveScene);
+        }
+    }
+}   
